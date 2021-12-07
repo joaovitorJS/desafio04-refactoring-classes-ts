7831133a96447d3f00ab91cf8294a8ab397013ed
joaoJS-Love <joao.vitordeoliveira@hotmail.com>
2021-12-07 09:43:52 -0400

    changing-to-ts-ModalAddFood-and-Input-component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,11 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
 
@@ -20,6 +25,8 @@ export const Container = styled.div`
     font-size: 36px;
     line-height: 36px;
   }
+
+
 
   ${props =>
     props.isFocused &&
